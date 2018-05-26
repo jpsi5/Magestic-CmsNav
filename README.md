0ffec00485708013eb6114ebc07c94ca5ae68375
Justin Simon <justin.simon0019@gmail.com>
2018-05-26 15:34:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+Magestic CmsNav Extension
+=====================
+CMS Page Navigation Links 
+Facts
+-----
+- version: 1.0.0
+- extension key: Magestic_CmsNav
+- [extension on Magento Connect](http://www.magentocommerce.com/magento-connect/magestic)-cmsnav-1234.html)
+- Magento Connect 1.0 extension key: magento-community/Magestic_CmsNav
+- Magento Connect 2.0 extension key: http://connect20.magentocommerce.com/community/Magestic_CmsNav
+- [extension on GitHub](https://github.com/magestic/Magestic_CmsNav)
+- [direct download link](http://connect.magentocommerce.com/community/get/Magestic_CmsNav-1.0.0.tgz)
+
+Description
+-----------
+This module includes the feature to Add CMS page links to the navigation menu.
+
+
+Requirements
+------------
+- PHP >= 5.5.0
+
+Compatibility
+-------------
+- Magento >= 1.9
+
+Installation Instructions
+-------------------------
+1. Install the extension via Magento Connect with the key shown above or copy all the files into your document root.
+2. Clear the cache, logout from the admin panel and then login again.
+3. Configure and activate the extension under System - Configuration - Company - Example Extension.
+4. ...
+
+Uninstallation
+--------------
+1. Remove all extension files from your Magento installation
+2. ...
+
+Support
+-------
+If you have any issues with this extension, open an issue on [GitHub](https://github.com/magestic/Magestic_CmsNav/issues).
+
+Contribution
+------------
+Any contribution is highly appreciated. The best way to contribute code is to open a [pull request on GitHub](https://help.github.com/articles/using-pull-requests).
+
+Developer
+---------
+
+[http://www.firstname-lastname.com](http://www.firstname-lastname.com)
+[@FirstNameLastName](https://twitter.com/FirstNameLastName)
+
+Licence
+-------
+[OSL - Open Software Licence 3.0](http://opensource.org/licenses/osl-3.0.php)
+
+Copyright
+---------
+(c) 2018 Magestic
---
+++ app/code/community/Magestic/CmsNav/Model/Observer.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Magento Enterprise Edition
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Magento Enterprise Edition End User License Agreement
+ * that is bundled with this package in the file LICENSE_EE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.magento.com/license/enterprise-edition
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magento.com for more information.
+ *
+ * @category    Magestic
+ * @package     Magestic_CmsNav
+ * @copyright Copyright (c) 2006-2018 Magento, Inc. (http://www.magento.com)
+ * @license http://www.magento.com/license/enterprise-edition
+ */
+
+/**
+ * Cms page edit form main tab
+ *
+ * @category    Magestic
+ * @package     Magestic_CmsNav
+ * @author      Justin Simon <justin.simon0019@gmail.com>
+ */
+class Magestic_CmsNav_Model_Observer
+{
+	/**
+     * Adding custom field to Main tab for navigation menu display option
+     *
+     * @param Varien_Event_Observer $observer
+     * @return Enterprise_Cms_Model_Observer
+     */
+	public function onMainTabPrepareForm(Varien_Event_Observer $observer)
+	{
+        $form = $observer->getEvent()->getForm();
+        $baseFieldset = $form->getElement('base_fieldset');
+
+        $page = Mage::registry('cms_page');
+
+        if ($page) {
+        	$baseFieldset->addField('show_in_menu', 'select', array(
+                'label'     => Mage::helper('adminhtml')->__('Visible In Navigation Menu'),
+                'title'     => Mage::helper('adminhtml')->__('Visible In Navigation Menu'),
+                'name'      => 'show_in_menu',
+                'values'    => Mage::getSingleton('adminhtml/system_config_source_yesno')->toOptionArray()
+            ),'identifier');
+        }
+
+        return $this;
+	}
+
+    /**
+     * Adds catalog categories to top menu
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function addCmsPageToTopmenuItems(Varien_Event_Observer $observer)
+    {
+        // $block = $observer->getEvent()->getBlock();
+        // $block->addCacheTag(Mage_Catalog_Model_Category::CACHE_TAG);
+        $storeId = Mage::app()->getStore()->getStoreId();
+        $this->_addCmsPagesToMenu(
+            Mage::getModel('cms/page')->getCollection()->addStoreFilter($storeId), $observer->getMenu());
+    }
+
+    /**
+     * Recursively adds categories to top menu
+     *
+     * @param Mage_Cms_Model_Page_Collection|array $pages
+     * @param Varien_Data_Tree_Node $parentNode
+     */
+    protected function _addCmsPagesToMenu($pages, $parentNode)
+    {
+        foreach ($pages as $page) {
+            if (!$page->getShowInMenu()) {
+                continue;
+            }
+
+            $nodeId = 'cms-node-' . $page->getPageId();
+
+            $tree = $parentNode->getTree();
+            $pageData = array(
+                'name' => $page->getTitle(),
+                'id' => $nodeId,
+                'url' => Mage::getBaseUrl() . $page->getIdentifier() . '/',
+                'is_active' => $page->getShowInMenu()
+            );
+            $pageNode = new Varien_Data_Tree_Node($pageData, 'id', $tree, $parentNode);
+            $parentNode->addChild($pageNode);
+        }
+    }
+}
